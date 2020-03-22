b3ba1ea70bb8c664f8d29f708cf6235e618bb7bb
Appleboyy <56431278+Appleboyy@users.noreply.github.com>
2020-03-22 23:38:24 +0800

    add facilities

---
+++ app/src/main/java/com/example/sgsports/Facility.java
@@ -0,0 +1,35 @@
+package com.example.sgsports;
+
+public class Facility {
+
+    private String Name;
+    private Double Latitude;
+    private Double Longitude;
+
+    public Facility() {
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+
+    public Double getLatitude() {
+        return Latitude;
+    }
+
+    public void setLatitude(Double latitude) {
+        Latitude = latitude;
+    }
+
+    public Double getLongitude() {
+        return Longitude;
+    }
+
+    public void setLongitude(Double longitude) {
+        Longitude = longitude;
+    }
+}
