0c43fc976a9a3615f199b98bc1d46d51c6657bee
blodvan1 <lkoh018@e.ntu.edu.sg>
2020-03-30 17:55:13 +0800

    report page

--- app/src/main/java/com/example/sgsports/ReportActivity.java
+++ app/src/main/java/com/example/sgsports/ReportActivity.java
@@ -4,12 +4,17 @@ package com.example.sgsports;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.text.DateFormat;
+import java.util.Collections;
 import java.util.Date;
+import java.lang.*;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.Spinner;
 import android.widget.TextView;
@@ -24,9 +29,17 @@ import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
+import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.ValueDependentColor;
+import com.jjoe64.graphview.helper.StaticLabelsFormatter;
+import com.jjoe64.graphview.series.BarGraphSeries;
+import com.jjoe64.graphview.series.DataPoint;
+import com.jjoe64.graphview.series.LineGraphSeries;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class ReportActivity extends AppCompatActivity {
 
@@ -36,12 +49,22 @@ public class ReportActivity extends AppCompatActivity {
     List<String> week;
     String weekZero, weekOne, weekTwo, weekThree, weekFour,weekFive,weekSix;
     String weekSelected, weekBefore, weekAfter;
-    TextView TvWeekSelected;
-    Double totalhours;
+    TextView TvWeekSelected , tvTotalHours, tvMoreThan, tvMostActiveDay,tvMostActivityHours;
+    Integer totalhours = 0 ,lastweekhours = 0 , morethanhours = 0, swimhours = 0, basketballhours = 0, gymhours = 0;
+    Integer monhrs = 0, tueshrs = 0, wedhrs = 0, thurshrs = 0, frihrs = 0;
+    Integer mon = 0, tues = 0, wed = 0, thurs = 0, fri = 0;
+    Integer swim = 0, basketball = 0, hockey, squash, tabletennis, badminton, stadium, gym = 0;
     String timeslot;
     String factype;
     String date;
+    String day;
     String apptInfo;
+    ImageView i;
+
+
+
+
+
 
     private FirebaseFirestore database;
     private FirebaseAuth mAuth;
@@ -55,6 +78,8 @@ public class ReportActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_report);
 
+
+        //initialise spinner items
         week = new ArrayList<String>();
         week.add("Week 1 of Mar, 2020");
         week.add("Week 2 of Mar, 2020");
@@ -63,15 +88,15 @@ public class ReportActivity extends AppCompatActivity {
         week.add("Week 1 of Apr, 2020");
 
         weekZero = "22/02/2020";
-        weekOne = "1/03/2020";
-        weekTwo = "9/03/2020";
+        weekOne = "01/03/2020";
+        weekTwo = "09/03/2020";
         weekThree = "16/03/2020";
         weekFour = "23/03/2020";
         weekFive = "30/03/2020";
         weekSix = "7/04/2020";
 
         //default, week is current week
-        weekSelected = weekOne;
+       // weekSelected = weekOne;
         ArrayAdapter<String> spinnerArrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, week);
         spinnerArrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 
@@ -88,33 +113,36 @@ public class ReportActivity extends AppCompatActivity {
                         weekBefore = weekZero;
                         weekSelected = weekOne;
                         weekAfter = weekTwo;
+                        showCurrentDateInfo(weekBefore, weekSelected, weekAfter);
                         break;
                     case 1:
                         weekBefore = weekOne;
                         weekSelected = weekTwo;
                         weekAfter = weekThree;
+                        showCurrentDateInfo(weekBefore, weekSelected, weekAfter);
                         break;
                     case 2:
                         weekBefore = weekOne;
                         weekSelected = weekThree;
                         weekAfter = weekFour;
+                        showCurrentDateInfo(weekBefore, weekSelected, weekAfter);
                         break;
                     case 3:
                         weekBefore = weekThree;
                         weekSelected = weekFour;
                         weekAfter = weekFive;
+                        showCurrentDateInfo(weekBefore, weekSelected, weekAfter);
                         break;
                     case 4:
                         weekBefore = weekFour;
                         weekSelected = weekFive;
                         weekAfter = weekSix;
+                        showCurrentDateInfo(weekBefore, weekSelected, weekAfter);
                         break;
 
                 }
 
-                TvWeekSelected = findViewById(R.id.weekSelected);
-                TvWeekSelected.setText(weekSelected);
-                 showCurrentDateInfo(weekBefore, weekSelected, weekAfter);
+
 
 
             }
@@ -138,11 +166,26 @@ public class ReportActivity extends AppCompatActivity {
         user = mAuth.getCurrentUser();
         userID = user.getUid();
         database = FirebaseFirestore.getInstance();
+        morethanhours = 0;
+        totalhours = 0;
+        lastweekhours = 0;
+        swimhours = 0;
+        basketballhours = 0;
+        gymhours = 0;
+        swim = 0; gym = 0; basketball = 0;
+        mon = 0; tues = 0; wed = 0; thurs = 0; fri = 0;
+        monhrs = 0; tueshrs = 0; wedhrs = 0; thurshrs = 0; frihrs = 0;
+
+        // Instantiate an ImageView and define its properties
 
 
-        listreview = (ListView) findViewById(R.id.reportList);
+
+
+
+
+        //listreview = (ListView) findViewById(R.id.reportList);
         final ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(ReportActivity.this, android.R.layout.simple_list_item_1, reportArray);
-        listreview.setAdapter(arrayAdapter);
+        //listreview.setAdapter(arrayAdapter);
 
 
         database.collection("mockAppointment")
@@ -157,6 +200,7 @@ public class ReportActivity extends AppCompatActivity {
                                 timeslot = document.getData().get("timeslot").toString();
                                 factype = document.getData().get("facType").toString();
                                 date = document.getData().get("date").toString();
+                                day = document.getData().get("Day").toString();
                                 apptInfo = timeslot + ", " + factype + '\n' + "Date: " + date + '\n';
                                 DateFormat df = new SimpleDateFormat("dd/MM/yyyy");
                                 try {
@@ -166,11 +210,180 @@ public class ReportActivity extends AppCompatActivity {
                                     Date appt_date = df.parse(date);
                                     Date prev_week = df.parse(weekBefore);
 
-                                    if (appt_date.before(end_date) && appt_date.after(start_date))
-                                        reportArray.add(apptInfo);
-                                    else if (appt_date.before(start_date) && appt_date.after(end_date))
-                                        lastweekreportArray.add(apptInfo);
-                                    arrayAdapter.notifyDataSetChanged();
+                                    //get most active activity
+
+                                    //get most active days
+                                    if (factype.equals("Swim")&& appt_date.before(end_date) && appt_date.after(start_date))
+                                    {
+                                        swim++;
+                                        swimhours++;
+                                    }
+                                    if (factype.equals("Basketball")&& appt_date.before(end_date) && appt_date.after(start_date))
+                                    {
+                                        basketball++;
+                                        basketballhours++;
+                                    }
+                                    if (factype.equals("Gym")&& appt_date.before(end_date) && appt_date.after(start_date))
+                                    {
+                                        gym++;
+                                        gymhours++;
+                                    }
+
+
+
+                                    if (day.contentEquals("Monday")&& appt_date.before(end_date) && appt_date.after(start_date))
+                                    {
+                                        mon++;
+                                        monhrs++;
+                                    }
+
+                                    if (day.equals("Tuesday")&& appt_date.before(end_date) && appt_date.after(start_date))
+                                    {
+                                        tues++;
+                                        tueshrs++;
+                                    }
+
+                                    if (day.equals("Wednesday")&& appt_date.before(end_date) && appt_date.after(start_date))
+                                    {
+                                        wed++;
+                                        wedhrs++;
+                                    }
+
+                                    if (day.equals("Thursday")&& appt_date.before(end_date) && appt_date.after(start_date))
+                                    {
+                                        thurs++;
+                                        thurshrs++;
+                                    }
+
+                                    if (day.contentEquals("Friday") && appt_date.before(end_date) && appt_date.after(start_date))
+                                    {
+                                        fri++;
+                                        frihrs++;
+                                    }
+
+                                    if (appt_date.before(end_date) && appt_date.after(start_date)) {
+                                        //reportArray.add(apptInfo);
+                                        totalhours = totalhours + 1;
+                                    }
+                                    if (appt_date.before(start_date) && appt_date.after(prev_week)) {
+                                        //lastweekreportArray.add(apptInfo);
+                                        lastweekhours = lastweekhours + 1;
+                                    }
+                                    //arrayAdapter.notifyDataSetChanged();
+                                    morethanhours = totalhours - lastweekhours;
+                                    //initialise all textview items
+                                    tvTotalHours = findViewById(R.id.tvTotalHours);
+                                    tvMoreThan = findViewById(R.id.tvMoreThan);
+                                    tvMostActiveDay = findViewById(R.id.tvMostActiveDay);
+                                    tvMostActivityHours = findViewById(R.id.tvMostActivityHours);
+
+                                    tvTotalHours.setText(totalhours.toString() + " Hours");
+
+                                    if (morethanhours < 0) {
+                                        morethanhours = Math.abs(morethanhours);
+                                        tvMoreThan.setText("(" + morethanhours.toString() + " hours less than last week)");
+                                    }
+                                    else
+                                    {
+                                        tvMoreThan.setText("(" + morethanhours.toString() + " hours more than last week)");
+                                    }
+
+                                    //find most active date
+                                    Map<String,Integer> vars = new HashMap<>();
+                                    // HashMap<String, Integer> vars = new HashMap<String,Integer>();
+                                    vars.put("Monday", new Integer(mon));
+                                    vars.put("Tuesday", new Integer(tues));
+                                    vars.put("Wednesday", new Integer(wed));
+                                    vars.put("Thursday", new Integer(thurs));
+                                    vars.put("Friday", new Integer(fri));
+                                    int max = Collections.max(vars.values());
+                                    String key = null;
+                                    //List<String> keys = new ArrayList<>();
+                                    for (Map.Entry<String, Integer> entry : vars.entrySet()) {
+                                        if (entry.getValue()==max) {
+                                            key = entry.getKey();
+                                        }
+                                    }
+                                    tvMostActiveDay.setText(key);
+
+                                    //find most done sport
+                                    Map<String,Integer> sports = new HashMap<>();
+                                    // HashMap<String, Integer> vars = new HashMap<String,Integer>();
+                                    sports.put("Gym", new Integer(gym));
+                                    sports.put("Swim", new Integer(swim));
+                                    sports.put("Basketball", new Integer(basketball));
+
+                                    int max2 = Collections.max(sports.values());
+                                    String key2 = null;
+                                    //List<String> keys = new ArrayList<>();
+                                    for (Map.Entry<String, Integer> entry2 : sports.entrySet()) {
+                                        if (entry2.getValue()==max2) {
+                                            key2 = entry2.getKey();
+                                        }
+                                    }
+
+                                    i = findViewById(R.id.imageView);
+
+                                    if (key2.equals("Gym")) {
+                                        tvMostActivityHours.setText(gymhours.toString() + " hours");
+                                        i.setImageResource(R.drawable.gym);
+                                    }
+                                    else if (key2.equals("Swim")) {
+                                        tvMostActivityHours.setText(swimhours.toString() + " hours");
+                                        i.setImageResource(R.drawable.swimming);
+                                    }
+                                    else {
+                                        tvMostActivityHours.setText(basketballhours.toString() + " hours");
+                                        i.setImageResource(R.drawable.bball);
+
+
+                                    }
+                                    //plot graph
+
+                                    if (appt_date.before(end_date) && appt_date.after(start_date)) {
+                                        GraphView barchart = (GraphView) findViewById(R.id.graphDays);
+                                        BarGraphSeries<DataPoint> barchat = new BarGraphSeries<DataPoint>(new DataPoint[] {
+                                                new DataPoint(1, monhrs),
+                                                new DataPoint(2,tueshrs),
+                                                new DataPoint(3,wedhrs),
+                                                new DataPoint(4,thurshrs),
+                                                new DataPoint(5,frihrs)
+                                        });
+
+                                        barchat.setSpacing(50); // 50% spacing between bars
+                                        barchat.setAnimated(true);
+                                        barchart.removeAllSeries();
+                                        barchart.addSeries(barchat);
+                                        StaticLabelsFormatter staticLabelsFormatter = new StaticLabelsFormatter(barchart);
+                                        staticLabelsFormatter.setHorizontalLabels(new String[] {"mon", "tues", "wed","thurs","fri"});
+                                        barchart.getGridLabelRenderer().setLabelFormatter(staticLabelsFormatter);
+
+                                        // set the viewport wider than the data, to have a nice view
+                                        barchart.getViewport().setMinX(1d);
+                                        barchart.getViewport().setMaxX(5d);
+                                        barchart.getViewport().setXAxisBoundsManual(true);
+                                        // styling
+                                        barchat.setValueDependentColor(new ValueDependentColor<DataPoint>() {
+                                            @Override
+                                            public int get(DataPoint data) {
+                                                return Color.rgb((int) data.getX()*255/4, (int) Math.abs(data.getY()*255/6), 100);
+                                            }
+                                        });
+
+
+                                        // draw values on top
+                                        barchat.setDrawValuesOnTop(true);
+                                        barchat.setValuesOnTopColor(Color.BLUE);
+                                        //series.setValuesOnTopSize(50);
+
+
+
+                                    }
+
+
+
+
+
 
                                 } catch (ParseException e) {
                                     e.printStackTrace();
@@ -181,12 +394,14 @@ public class ReportActivity extends AppCompatActivity {
                         }
 
 
-                    }
 
-                });
-
+                }
 
 
 
-    }}
+
+
+
+    });
+}}
 
