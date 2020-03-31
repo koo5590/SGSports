d62d4206e7c1275ea93e0380b70aa3fe4fa0f276
Hyunjung Kim <hyunjung5590@gmail.com>
2020-03-31 13:12:36 +0800

    splash screen

---
+++ app/src/main/java/com/example/sgsports/SplashActivity.java
@@ -0,0 +1,17 @@
+package com.example.sgsports;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+
+        Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+        startActivity(intent);
+        finish();
+    }
+}
